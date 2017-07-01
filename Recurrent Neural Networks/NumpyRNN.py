74197e84ef1be2151d0f0220ae1db663b874c23c
asras <asras28@gmail.com>
2017-07-01 23:32:52 +0200

    Fixed some serious mistakes in the numpy RNN. It still needs optimization.

--- Recurrent Neural Networks/NumpyRNN.py
+++ Recurrent Neural Networks/NumpyRNN.py
@@ -9,15 +9,15 @@ def Softmax(x):
 def EncOHToOnehot(x, dim):
 	A = np.zeros(dim)
 	A[x] = 1
-	return A
+	return np.array(A)
 
 
 class NumpyRNN():
 
-	def __init__(self, inputDim, outputDim, hiddenDim, bptt_truncate):
-		U = np.random.uniform(-1/np.sqrt(inputDim), 1/np.sqrt(inputDim), (hiddenDim, inputDim))
-		V = np.random.uniform(-1/np.sqrt(hiddenDim), 1/np.sqrt(hiddenDim), (outputDim, hiddenDim))
-		W = np.random.uniform(-1/np.sqrt(hiddenDim), 1/np.sqrt(hiddenDim), (hiddenDim, hiddenDim))
+	def __init__(self, inputDim, outputDim, hiddenDim, bptt_truncate=4):
+		self.U = np.random.uniform(-1/np.sqrt(inputDim), 1/np.sqrt(inputDim), (hiddenDim, inputDim))
+		self.V = np.random.uniform(-1/np.sqrt(hiddenDim), 1/np.sqrt(hiddenDim), (outputDim, hiddenDim))
+		self.W = np.random.uniform(-1/np.sqrt(hiddenDim), 1/np.sqrt(hiddenDim), (hiddenDim, hiddenDim))
 		self.bptt_truncate = bptt_truncate
 		self.inputDim = inputDim
 		self.outputDim = outputDim
@@ -28,19 +28,19 @@ class NumpyRNN():
 		U, W, V = self.U, self.W, self.V
 		#x is encoded one-hot, i.e. [0, 19, 222, 1290, 1]
 		o = []
-		s = [np.zeros(np.zeros(self.hiddenDim))]
+		s = [np.zeros(self.hiddenDim)]
 		for j in np.arange(len(x)):
 			act = U[:,x[j]] + W.dot(s[j])
 			st = np.tanh(act)
 			s.append(st)
 			o.append(Softmax(V.dot(st)))
-		return o
+		return np.array(o)
 
 	def FeedForwardForGradient(self, x, time):
 		U, W, V = self.U, self.W, self.V
-		#x is encoded one-hot, i.e. [0, 19, 222, 1290, 1]
+		#x is encoded one-hot, e.g. [0, 19, 222, 1290, 1]
 		o = []
-		s = [np.zeros(np.zeros(self.hiddenDim))]
+		s = [np.zeros(self.hiddenDim)]
 		acts = []
 		for j in np.arange(time+1):			
 			act = U[:,x[j]] + W.dot(s[j])
@@ -48,65 +48,88 @@ class NumpyRNN():
 			acts.append(act)
 			s.append(st)
 			o.append(Softmax(V.dot(st)))
-		return [o[-1], s[-1], act[-1]]
+		return [np.array(o[-1]), np.array(s[-1]), np.array(acts[-1])]
 
 
-	def dSdU(self, x, timestep):
-		if timestep == 0:
+	def dSdU(self, x, timestep,  bptttrunc):
+		if timestep == 0 or bptttrunc==0:
 			[o0, s0, act0] = self.FeedForwardForGradient(x, 0)
-			x0 = EncOHToOnehot(x[0])
+			x0 = EncOHToOnehot(x[0], self.inputDim)
 			nameNotFound = np.outer(np.ones(len(act0))-(np.tanh(act0)**2), x0)
 			return nameNotFound
-		[ot, st, actt] = self.FeedForwardForGradient(x[0:timestep], timestep)
-		xt = EncOHToOnehot(x[timestep])
-		dunnoName = self.W.dot(self.dSdU(x, y, timestep-1))
-		nameDunno = np.ones(len(actt))-(np.tanh(actt))**2
-		dSdU = np.outer(nameDunno, xt+dunnoName)
+		[ot, st, actt] = self.FeedForwardForGradient(x, timestep)
+		xt = EncOHToOnehot(x[timestep], self.inputDim)
+		dunnoName = self.W.dot(self.dSdU(x, timestep-1,  bptttrunc-1))
+		nameDunno = np.ones(len(actt))-(np.tanh(actt)**2)
+		dSdU = np.outer(nameDunno, xt)+np.diag(nameDunno).dot(dunnoName)
 		return dSdU
 
-	def dU(self, x, y, timestep):
-		yt = EncOHToOnehot(y[timestep])
-		ot = self.FeedForwardForGradient(x, timestep)
-		name404 = (ot-yt).dot(self.V).dot(self.dSdU)
+	def dU(self, x, y, timestep, bptttrunc):
+		yt = EncOHToOnehot(y[timestep], self.inputDim)
+		[ot, st, actt] = self.FeedForwardForGradient(x, timestep)
+		name404 = (ot-yt).dot(self.V).dot(self.dSdU(x, timestep,  bptttrunc))
 		return name404
 
 
-	def dV(self, x, y, timestep):
+	def dV(self, x, y, timestep, bptttrunc):
 		[ot, st, actt] = self.FeedForwardForGradient(x, timestep)
-		yt = EncOHToOnehot(y[timestep])
+		yt = EncOHToOnehot(y[timestep], self.inputDim)
 		return np.outer(ot-yt, st)
 
-	def dSdW(self, x, timestep):
-		if timestep == 0:
-			return np.zeros(self.hiddenDim, self.hiddenDim)
+	def dSdW(self, x, timestep,  bptttrunc):
+		if timestep == 0 or bptttrunc == 0:
+			return np.zeros((self.hiddenDim, self.hiddenDim))
 
 		[ot, st, actt] = self.FeedForwardForGradient(x, timestep)
 		[opt, spt, actpt] = self.FeedForwardForGradient(x, timestep-1)
 		diffActs = np.ones(len(actt))-np.tanh(actt)**2
-		aGreatName = spt + self.W.dot(self.dSdW(x, timestep-1))
-		return np.outer(diffActs, aGreatName)
+		aGreatName = self.W.dot(self.dSdW(x, timestep-1,  bptttrunc-1))
+		return np.outer(diffActs, spt) + np.diag(diffActs).dot(aGreatName)
 
-	def dW(self, x, y, timestep):
+	def dW(self, x, y, timestep, bptttrunc):
 		[ot, st, actt] = self.FeedForwardForGradient(x, timestep)
-		yt = EncOHToOnehot(y[timestep])
-		firstThing = (ot-yt).dot(self.V).self.dSdW
+		yt = EncOHToOnehot(y[timestep], self.inputDim)
+		firstThing = (ot-yt).dot(self.V).dot(self.dSdW(x, timestep,  bptttrunc))
 		return firstThing
 
 
 	def SGTrain(self, x, y, learningRate):
-		dU = np.zeros(self.hiddenDim, self.inputDim)
-		dW = np.zeros(self.hiddenDim, self.hiddenDim)
-		dV = np.zeros(self.outputDim, self.hiddenDim)
+		dU = np.zeros((self.hiddenDim, self.inputDim))
+		dW = np.zeros((self.hiddenDim, self.hiddenDim))
+		dV = np.zeros((self.outputDim, self.hiddenDim))
 		for t in np.arange(len(x)):
-			dU += self.dU(x, y, t)
-			dW += self.dW(x, y, t)
-			dV += self.dV(x, y, t)
+			dU += self.dU(x, y, t, self.bptt_truncate)
+			dW += self.dW(x, y, t, self.bptt_truncate)
+			dV += self.dV(x, y, t, self.bptt_truncate)
 
-		self.U += learningRate*dU
-		self.W += learningRate*dW
-		self.V += learningRate*dV
-		
+		self.U += -learningRate*dU
+		self.W += -learningRate*dW
+		self.V += -learningRate*dV
 
+
+
+
+
+if __name__=='__main__':
+	t = datetime.datetime.now().time()
+	print('Program started at', t)
+	[word_to_index, index_to_word, X_train, Y_train] = load_training_data('NumpyRNNData.csv', 'NumpyRNNwordtofromindex.npz')
+	word_dim = len(word_to_index)
+	NN = NumpyRNN(word_dim, word_dim, 100, 4)
+	Numpy_load_model_parameters('NumpySavedParameters.npz', NN)
+	print('Training commenced.')
+	t1 = time.time()
+	for j in np.arange(1):
+		NN.SGTrain(X_train[j], Y_train[j], 0.001)
+	t2 = time.time()
+	print('Training took ' + str(t2-t1) + ' seconds.')
+
+	Numpy_save_model_parameters('NumpySavedParameters', NN)
+
+
+
+	timeend = datetime.datetime.now().time()
+	print('Program terminated at', timeend)
 
 
 
--- Recurrent Neural Networks/tools.py
+++ Recurrent Neural Networks/tools.py
@@ -1,5 +1,16 @@
 import numpy as np
+import csv
 
+def load_training_data(filename, dictfilename):
+	with open(filename, 'r', newline='', encoding='utf-8') as f:
+		reader = csv.reader(f)
+		sentences = [s for s in reader]
+	npzfile = np.load(dictfilename)
+	word_to_index = npzfile['wtoi'][()]
+	index_to_word = npzfile['itow']
+	X_train = np.asarray([[word_to_index[w] for w in sent[:-1]] for sent in sentences])
+	Y_train = np.asarray([[word_to_index[w] for w in sent[1:]] for sent in sentences])
+	return [word_to_index, index_to_word, X_train, Y_train]
 
 def softmax(x):
 	xt = np.exp(x-np.max(x))
@@ -45,4 +56,21 @@ def LSTM_load_model_parameters(path, model):
 	model.Uo.set_value(Uo)
 	model.Wo.set_value(Wo)
 	model.V.set_value(V)
+	print('Loaded model parameters.')
+
+
+def Numpy_save_model_parameters(outfile, model):
+	U, V, W = model.U, model.V, model.W
+	np.savez(outfile, U=U, V=V, W=W)
+	print('Saved model parameters to %s.' % outfile)
+
+
+def Numpy_load_model_parameters(path, model):
+	npzfile = np.load(path)
+	U, V, W = npzfile['U'], npzfile['V'], npzfile['W']
+	#model.hidden_dim = U.shape[0]
+	#model.word_dim = U.shape[1]
+	model.U = U
+	model.V = V
+	model.W = W
 	print('Loaded model parameters.')
